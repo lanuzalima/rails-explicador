ecbcf140fc80a42ba44f93027b997eaa0ca6fd21
Gustavo Sousa <gussousa@gmail.com>
2020-12-03 16:28:47 -0300

    Modelo de lecture criado

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_175406) do
+ActiveRecord::Schema.define(version: 2020_12_03_192745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lectures", force: :cascade do |t|
+    t.string "title"
+    t.string "subject"
+    t.text "description"
+    t.integer "duration"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lectures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2020_12_03_175406) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lectures", "users"
 end
