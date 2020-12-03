208c0bf9413fbeb55f24fd4a0337c9fdddd8c422
lanuzalima <lanuzalima@gmail.com>
2020-12-03 18:33:11 -0300

    jornada criar aula pronta

---
+++ app/controllers/lectures_controller.rb
@@ -0,0 +1,30 @@
+class LecturesController < ApplicationController
+  def index
+    @lectures = Lecture.all
+  end
+
+  def new
+    @lecture = Lecture.new
+  end
+
+  def show
+    @lecture = Lecture.find(params[:id])
+  end
+
+  def create
+    @lecture = Lecture.new(lecture_params)
+    @lecture.user = current_user
+
+    if @lecture.save
+      redirect_to lecture_path(@lecture)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def lecture_params
+    params.require(:lecture).permit(:title, :subject, :description, :duration)
+  end
+end
