27ecd84c15b5618a2dec23bd9fcbc11c911b11c9
Rafael Silva <rafaelvds@gmail.com>
2020-12-04 17:29:00 -0300

    view de booking criada

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :availability
   belongs_to :user
+
+  has_one :lecture, through: :availability
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,4 +1,5 @@
 class Lecture < ApplicationRecord
   has_many :availabilities, dependent: :destroy
   belongs_to :user
+  has_many :bookings, through: :availabilities
 end
