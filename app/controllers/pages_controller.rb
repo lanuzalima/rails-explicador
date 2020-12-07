fe1d19d9c2d11840978c5bc67c37b481a0ca5dfc
Virginia Silva <silvavmo@stj.jus.br>
2020-12-07 15:39:09 -0300

    home versão inicial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @lectures = Lecture.all
+  end
+
+  def show
+    @lecture = Lecture.find(params[:id])
+    @availabilities = Availability.where(lecture_id: @lecture.id)
+    @owner = Lecture.find(params[:id]).user
   end
 end
