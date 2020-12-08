9d59052811f73cc7adc68ed330bcf31cc9e314a3
Gustavo Sousa <gussousa@gmail.com>
2020-12-08 14:28:16 -0300

    Página de show da lecture reformada.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,25 +7,44 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
     authorize @booking
     if book_valid?
       if @booking.save
-        lecture_id = @booking.lecture.id
-        redirect_to lecture_availability_booking_path(id: @booking.id,
-                                                      lecture_id: lecture_id,
-                                                      availability_id: @booking.availability_id),
+        lecture = @booking.availability.lecture
+        redirect_to lecture_path(lecture),
                     notice: 'Aula cadastrada com sucesso!'
       else
-        redirect_to lecture_availability_path(id: params[:availability_id],
-                                              lecture_id: params[:lecture_id]),
+        redirect_to lecture_path(lecture),
                     notice: 'Aula indisponível para reserva. Por favor, escolha outro horário.'
       end
     else
-      redirect_to lecture_availability_path(id: params[:availability_id],
-                                            lecture_id: params[:lecture_id]),
+      redirect_to lecture_path(lecture),
                   alert: 'Horário já agendado. Por favor, escolha outro horário.'
     end
   end
+
+  # def create
+  #   @booking = Booking.new(booking_params)
+  #   authorize @booking
+  #   if book_valid?
+  #     if @booking.save
+  #       lecture_id = @booking.lecture.id
+  #       redirect_to lecture_availability_booking_path(id: @booking.id,
+  #                                                     lecture_id: lecture_id,
+  #                                                     availability_id: @booking.availability_id),
+  #                   notice: 'Aula cadastrada com sucesso!'
+  #     else
+  #       redirect_to lecture_availability_path(id: params[:availability_id],
+  #                                             lecture_id: params[:lecture_id]),
+  #                   notice: 'Aula indisponível para reserva. Por favor, escolha outro horário.'
+  #     end
+  #   else
+  #     redirect_to lecture_availability_path(id: params[:availability_id],
+  #                                           lecture_id: params[:lecture_id]),
+  #                 alert: 'Horário já agendado. Por favor, escolha outro horário.'
+  #   end
+  # end
 
   def show
   end
